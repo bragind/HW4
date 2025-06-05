3d54770757c5a7df327b565d7ff5d7e6f290e618
BraginD <dimanb1982@gmail.com>
2025-06-05 14:03:36 +0300

    modified:   CMakeLists.txt 	modified:   build/CMakeCache.txt 	new file:   build/CMakeFiles/4.0.2/CMakeCCompiler.cmake 	new file:   build/CMakeFiles/4.0.2/CMakeDetermineCompilerABI_C.bin 	modified:   build/CMakeFiles/4.0.2/CMakeDetermineCompilerABI_CXX.bin 	new file:   build/CMakeFiles/4.0.2/CompilerIdC/CMakeCCompilerId.c 	new file:   build/CMakeFiles/4.0.2/CompilerIdC/a.exe 	modified:   build/CMakeFiles/4.0.2/CompilerIdCXX/a.exe 	modified:   build/CMakeFiles/CMakeConfigureLog.yaml 	new file:   build/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/CMakeFiles/HW4.dir/DependInfo.cmake 	new file:   build/CMakeFiles/HW4.dir/build.make 	new file:   build/CMakeFiles/HW4.dir/cmake_clean.cmake 	new file:   build/CMakeFiles/HW4.dir/cmake_clean_target.cmake 	new file:   build/CMakeFiles/HW4.dir/compiler_depend.make 	new file:   build/CMakeFiles/HW4.dir/compiler_depend.ts 	new file:   build/CMakeFiles/HW4.dir/depend.make 	new file:   build/CMakeFiles/HW4.dir/flags.make 	new file:   build/CMakeFiles/HW4.dir/includes_CXX.rsp 	new file:   build/CMakeFiles/HW4.dir/link.txt 	new file:   build/CMakeFiles/HW4.dir/progress.make 	new file:   build/CMakeFiles/HW4.dir/src/point.cpp.obj 	new file:   build/CMakeFiles/HW4.dir/src/point.cpp.obj.d 	new file:   build/CMakeFiles/HW4.dir/src/polygon.cpp.obj 	new file:   build/CMakeFiles/HW4.dir/src/polygon.cpp.obj.d 	new file:   build/CMakeFiles/InstallScripts.json 	new file:   build/CMakeFiles/Makefile.cmake 	new file:   build/CMakeFiles/Makefile2 	new file:   build/CMakeFiles/TargetDirectories.txt 	new file:   build/CMakeFiles/app.dir/DependInfo.cmake 	new file:   build/CMakeFiles/app.dir/app/main.cpp.obj 	new file:   build/CMakeFiles/app.dir/app/main.cpp.obj.d 	new file:   build/CMakeFiles/app.dir/build.make 	new file:   build/CMakeFiles/app.dir/cmake_clean.cmake 	new file:   build/CMakeFiles/app.dir/compiler_depend.make 	new file:   build/CMakeFiles/app.dir/compiler_depend.ts 	new file:   build/CMakeFiles/app.dir/depend.make 	new file:   build/CMakeFiles/app.dir/flags.make 	new file:   build/CMakeFiles/app.dir/includes_CXX.rsp 	new file:   build/CMakeFiles/app.dir/link.txt 	new file:   build/CMakeFiles/app.dir/linkLibs.rsp 	new file:   build/CMakeFiles/app.dir/objects.a 	new file:   build/CMakeFiles/app.dir/objects1.rsp 	new file:   build/CMakeFiles/app.dir/progress.make 	new file:   build/CMakeFiles/progress.marks 	new file:   build/Makefile 	new file:   build/app.exe 	new file:   build/cmake_install.cmake 	new file:   build/libHW4.a

---
+++ build/CMakeFiles/HW4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW4.dir/src/point.cpp.obj"
+  "CMakeFiles/HW4.dir/src/point.cpp.obj.d"
+  "CMakeFiles/HW4.dir/src/polygon.cpp.obj"
+  "CMakeFiles/HW4.dir/src/polygon.cpp.obj.d"
+  "libHW4.a"
+  "libHW4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
